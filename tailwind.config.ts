a230f6952db1c4e52d0d55c280ba4d3b0986695b
Masaki Kusaka <masaki.kusaka@example.com>
2025-01-18 17:22:43 +0900

    Move JS animations to public/js and reference them in _app.tsx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // ★ ここでpagesフォルダ or src/pagesフォルダなどを指定。
-  //   今回はpages/に_app.tsxやindex.tsxがある想定で「./pages」も含める
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
+export default config;
